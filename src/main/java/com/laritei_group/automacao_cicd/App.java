a1e26687ef496992976b444a30037471329d9efb
LariTei <larisilva.018@gmail.com>
2026-06-27 14:43:44 -0300

    Teste Automação Java

---
+++ src/main/java/com/laritei_group/automacao_cicd/App.java
@@ -0,0 +1,10 @@
+package com.laritei_group.automacao_cicd;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/laritei_group/automacao_cicd/AppTest.java
@@ -0,0 +1,18 @@
+package com.laritei_group.automacao_cicd;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
