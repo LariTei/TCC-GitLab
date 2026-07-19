7e7f555db615f285ec93694bbe8fbe2e078e1493
LariTei <larisilva.018@gmail.com>
2026-07-19 18:52:25 -0300

    Tentando rodar o teste na pipeline

--- test/test_mecanica.py
+++ test/test_mecanica.py
@@ -23,7 +23,7 @@ class TestOficinaInterface(LiveServerTestCase):
         cls.p = cls.playwright_manager.__enter__()
 
         # Lança o navegador e cria a página compartilhada
-        cls.browser = cls.p.chromium.launch(headless=true)
+        cls.browser = cls.p.chromium.launch(headless=True)
         cls.page = cls.browser.new_page()
 
     @classmethod
